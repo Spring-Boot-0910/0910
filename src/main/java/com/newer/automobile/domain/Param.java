d8df83a46ed56da79142c65e9e12df84db5253a4
cttisapig <2469228898@qq.com>
2019-09-27 15:51:39 +0800

    SpringSecurity

---
+++ src/main/java/com/newer/automobile/domain/Param.java
@@ -0,0 +1,42 @@
+package com.newer.automobile.domain;
+
+import java.util.List;
+
+public class Param {
+    private List<?> data;
+    private Integer iTotalDisplayRecords;
+    private Integer iTotalRecords;
+
+    public Param() {
+    }
+
+    public Param(List<?> data, Integer iTotalDisplayRecords, Integer iTotalRecords) {
+        this.data = data;
+        this.iTotalDisplayRecords = iTotalDisplayRecords;
+        this.iTotalRecords = iTotalRecords;
+    }
+
+    public List<?> getData() {
+        return data;
+    }
+
+    public void setData(List<?> data) {
+        this.data = data;
+    }
+
+    public Integer getiTotalDisplayRecords() {
+        return iTotalDisplayRecords;
+    }
+
+    public void setiTotalDisplayRecords(Integer iTotalDisplayRecords) {
+        this.iTotalDisplayRecords = iTotalDisplayRecords;
+    }
+
+    public Integer getiTotalRecords() {
+        return iTotalRecords;
+    }
+
+    public void setiTotalRecords(Integer iTotalRecords) {
+        this.iTotalRecords = iTotalRecords;
+    }
+}
